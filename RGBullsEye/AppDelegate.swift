936fd0bc427d301d4dc97933bf1deabc93dbe6ac
Jan Hovland <jan.hovland@lyse.net>
2020-04-01 10:30:53 +0200

    Update

--- RGBullsEye/AppDelegate.swift
+++ RGBullsEye/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
 
         // Create the window and set the content view. 
         window = NSWindow(
--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,15 +9,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+
+    @State var showAlert = false
+
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1.0 - diff) * 100.0 + 0.5)
     }
+
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    //                    Text("Match this color")
+                    self.showAlert ? Text("R: \(Int(rTarget * 255.0))" + " G: \(Int(gTarget * 255.0))"
+                        + " B: \(Int(bTarget * 255.0))")
+                        : Text("Match this color")
+                }
+
+                VStack {
+                    ZStack(alignment: .center) {
+                        Color(red: rGuess, green: gGuess, blue: bGuess)
+                        Text("60")
+                            .padding(.all, 5)
+                            .background(Color.white)
+                            .mask(Circle())
+                            .foregroundColor(.black)
+                    }
+                    Text("R: 127  G: 127  B: 127")
+                }
+            }
+            .padding(.horizontal)
+            .padding(.vertical)
+            Button(action: {
+                self.showAlert = true
+            }) {
+                Text("Hit me!")
+
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"),
+                      message: Text(String(computeScore())))
+            }
+            .padding(.horizontal)
+
+            VStack {
+                ExtractedView(value: $rGuess, textColor: .red)
+                ExtractedView(value: $gGuess, textColor: .green)
+                ExtractedView(value: $bGuess, textColor: .blue)
+            }
+            .padding(.vertical)
+            .padding(.horizontal)
+        }
+    }
+
 }
 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ExtractedView: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+                .background(textColor)
+                .cornerRadius(10)
+            Text("255").foregroundColor(textColor)
+        }
+        .padding(.horizontal)
     }
 }
